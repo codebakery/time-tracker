2575a98d5466a222ad4619d805a713b83cf39356
tsh <dr.tallin@gmail.com>
2016-09-30 16:48:22 +0300

    add admin registration app

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AdminPasswordChangeForm
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from . import models
+
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('username', 'email', 'timestamp')
+    list_display_links = ('username', 'email',)
+    #change_list_template = 'admin/registration/list_users.html'
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+    fieldsets = None
+    
+admin.site.register(models.User, UserAdmin)
+
+admin.site.unregister(Group)
