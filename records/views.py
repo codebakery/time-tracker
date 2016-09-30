88cca96f2a5bd73af4cd3757c0a26d111347d84c
tsh <dr.tallin@gmail.com>
2016-09-30 16:24:32 +0300

    add projects crud

--- records/views.py
+++ records/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render_to_response
 from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Record
-from .serializers import RecordSerializer
+from .models import Record, Project
+from .serializers import RecordSerializer, ProjectSerializer
 
 User = get_user_model()
 
@@ -13,4 +13,10 @@ User = get_user_model()
 class Records(generics.ListCreateAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
+    permission_classes = (IsAuthenticated,)
+    
+
+class Projects(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
     permission_classes = (IsAuthenticated,)
