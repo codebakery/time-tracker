f5f3cf555c15bb5e6f0d14ad94a449c7cfeadd6d
tsh <dr.tallin@gmail.com>
2016-10-03 17:18:28 +0300

    set password on user

--- registration/serializers.py
+++ registration/serializers.py
@@ -11,3 +11,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'password')
         extra_kwargs = {'password': {'write_only': True,}}
+        
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        if validated_data.get('password'):
+            user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            pwd = validated_data.pop('password')
+            instance.set_password(pwd)
+            instance.save()
+        return super(UserSerializer, self).update(instance, validated_data)
